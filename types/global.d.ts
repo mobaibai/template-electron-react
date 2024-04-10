ec9bb526242104d2a0a1574141c4cf6c3f5b123a
mobaibai <puresilent.com@gmail.com>
2024-04-10 10:13:52 +0800

    🎈 perf: 迁移类型声明

--- types/global.d.ts
+++ types/global.d.ts
@@ -17,4 +17,37 @@ declare global {
       start: () => void
     }
   }
+
+  interface SystemInfo {
+    key: string
+    name: string
+    value: string | T[]
+  }
+
+  type isDev = boolean
+
+  type JSONValue = string | number | boolean | null | { [k: string]: JSONValue } | JSONValue[]
+
+  type RequestQueryType = {
+    query: Record<string, string>
+  }
+
+  type ItemType = {
+    id?: string | number
+  }
+
+  type ResponseDataListType = {
+    list: ItemType[]
+    query: E
+    pages: {
+      page: number
+      per: number
+      total_page: number
+    }
+  }
+
+  type DataType<R> = {
+    code: number
+    data: E
+  }
 }
